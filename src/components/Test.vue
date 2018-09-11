bf1d770924681d0ed602a3c54d25554a6bcb0c73
Fabian Orellana <fabiorellana95@gmail.com>
2018-09-11 03:38:26 -0300

    First commit

---
+++ src/components/Test.vue
@@ -0,0 +1,67 @@
+<template>
+	<div class="test">
+		<!--<div v-text="title"></div>
+		<div v-if="showName">
+			<p>{{ user.firstName }}</p>
+		</div>
+		<div v-else>
+			<p>No puedo mostrar el nombre</p>
+		</div>
+		<hr>
+		<ul>
+			<li v-for="task in tasks">
+				{{ task.title }}
+			</li>
+		</ul>
+		<br>-->
+		<!--<input type="text" v-on:keyup.enter="typedEnter">-->
+		{{ msg }}
+	</div>
+</template>
+
+<script>
+	export default{
+		data() {
+			return {
+				title: 'Hola VueJs',
+				user : {
+					firstName: 'Fabian',
+					lastName: 'Orellana'
+				},
+				myHtml: '<h3>Hola vue</h3>',
+				showName: false,
+				tasks: [
+					{title: 'eat'},
+					{title: 'dinner'},
+					{title: 'play'},
+					{title: 'task'}
+				]
+			}
+		},
+		methods: {
+			saludar(name) {
+				alert('Wena ' + name)
+			},
+			typedEnter(e) {
+				console.log('DIO UN ENTER');
+			}
+		},
+		computed: {
+			fullName() {
+				return this.user.firstName + ' ' + this.user.lastName;
+			}
+		},
+		props: {
+			msg: {
+				type: String,
+				default: 'Mensaje por defecto'
+			}
+		}
+	}
+</script>
+
+<style media="screen">
+	.test{
+		background-color: #eee;
+	}
+</style>
---
+++ src/components/User.vue
@@ -0,0 +1,50 @@
+<template lang="html">
+	<div class="users">
+		<h1>User component</h1>
+		<ul>
+			<li v-for="user in users">
+				{{ user.name }} - {{ user.email }} <button v-on:click="deleteUser(user)">X</button>
+			</li>
+		</ul>
+
+		<form v-on:submit.prevent="addUser">
+			<input type="text" v-model="newUser.name" placeholder="Nombre">
+			<input type="email" v-model="newUser.email" placeholder="Email">
+			<button type="submit">
+				Add
+			</button>
+		</form>
+	</div>
+</template>
+
+<script>
+	export default {
+		data (){
+			return {
+				users: [],
+				newUser: {}
+			}
+		},
+		methods: {
+			addUser(){
+				this.users.push(this.newUser);
+				this.newUser = {};
+			},
+			deleteUser(user){
+				this.users.splice(this.users.indexOf(user), 1);
+			}
+		},
+		created(){
+			this.$http.get('https://jsonplaceholder.typicode.com/users')
+			.then(res => this.users = res.body);
+		}
+	}
+</script>
+
+<style lang="css">
+	.users {
+		background: #333;
+		color: #fff;
+		padding: 20px;
+	}
+</style>
